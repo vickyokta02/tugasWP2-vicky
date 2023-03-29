56f9f2f4a87dbd32d916f1f49ac09b61faf86018
vickyokta02 <voktadiyanto@gmail.com>
2023-03-29 17:25:43 +0700

    menambahkan application/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Vicky Oktadiyanto
+             Saya Tinggal di daerah Sunter Jaya,Olahraga yang disukai adalah sepakbola";
+             
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan..yuk Kita bealajr web programming ! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari permodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $hasil; ?>
+
+</body>
+
+</html>
