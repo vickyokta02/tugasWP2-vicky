a02d3916084deb941f4c19662b32b4454e6a8348
vickyokta02 <voktadiyanto@gmail.com>
2023-03-30 21:10:52 +0700

    Menambahkan pustaka-booking

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan..yuk Kita belajar web programming ! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari permodelan dengan methode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+
+</body>
+
+</html>
