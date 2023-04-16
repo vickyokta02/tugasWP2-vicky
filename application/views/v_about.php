a5a423bc881bb341614249f5196286b9d73b430a
vickyokta02 <voktadiyanto@gmail.com>
2023-04-16 20:48:04 +0700

    Tugas 2 WP

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama : Vicky Oktadiyanto</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Gg.SKy 7 Sunter Jaya No.128 Tanjung Priok</li>
+    </ul>
+        
+    <h4>Tempat/Tanggal Lahir</h4>
+    <ul type="none">
+        <li>Pemalang 02 Oktober 2001</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Game Online</li>
+        <li>Sepakbola</li>
+    </ul>
+</section>
